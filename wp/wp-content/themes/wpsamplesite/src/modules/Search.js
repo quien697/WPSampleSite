f1518202b0ff36c403ed3b2dd61ec5553fff2971
Quien <quien697@gmail.com>
2024-06-18 10:52:39 -0700

    refactor: update JavaScript without jQuery

--- wp/wp-content/themes/wpsamplesite/src/modules/Search.js
+++ wp/wp-content/themes/wpsamplesite/src/modules/Search.js
@@ -1,13 +1,13 @@
-import $ from "jquery";
+import axios from "axios";
 
 class Search {
     constructor() {
         this.addSearchHTML();
-        this.openButton = $(".js-search-trigger");
-        this.closeButton = $(".search-overlay__close");
-        this.resultsDiv = $("#search-overlay__results");
-        this.searchOverlay = $(".search-overlay");
-        this.searchField = $("#search-term");
+        this.openButton = document.querySelectorAll(".js-search-trigger");
+        this.closeButton = document.querySelector(".search-overlay__close");
+        this.resultsDiv = document.querySelector("#search-overlay__results");
+        this.searchOverlay = document.querySelector(".search-overlay");
+        this.searchField = document.querySelector("#search-term");
         this.isOverlayOpen = false;
         this.isSpinnerVisible = false;
         this.previousValue = null;
@@ -16,15 +16,24 @@ class Search {
     }
 
     events() {
-        this.openButton.on("click", this.openOverlay.bind(this));
-        this.closeButton.on("click", this.closeOverlay.bind(this));
-        $(document).on("keydown", this.keyPressDispatcher.bind(this));
-        this.searchField.on("keydown", this.typingLogic.bind(this));
+        this.openButton.forEach(element => {
+            element.addEventListener("click", e => {
+                e.preventDefault();
+                this.openOverlay();
+            });
+        });
+        this.closeButton.addEventListener("click", () => this.closeOverlay());
+        this.searchField.addEventListener("keyup", () => this.typingLogic());
+        document.addEventListener("keydown", e => this.keyPressDispatcher(e));
     }
 
     keyPressDispatcher(e) {
-        if (e.keyCode === 83 && !this.isOverlayOpen && !$("input, textarea").is(":focus")) {
-            this.openOverlay()
+        if (
+            e.keyCode === 83 && !this.isOverlayOpen &&
+            document.activeElement.tagName !== "INPUT" &&
+            document.activeElement.tagName !== "TEXTAREA"
+        ) {
+            this.openOverlay();
         }
         if (e.keyCode === 27 && this.isOverlayOpen) {
             this.closeOverlay();
@@ -32,27 +41,28 @@ class Search {
     }
 
     typingLogic() {
-        if (this.searchField.val() !== this.previousValue) {
-            clearTimeout(this.typingTimer)
-
-            if (this.searchField.val()) {
+        if (this.searchField.value !== this.previousValue) {
+            clearTimeout(this.typingTimer);
+            if (this.searchField.value) {
                 if (!this.isSpinnerVisible) {
-                    this.resultsDiv.html('<div class="spinner-loader"></div>');
+                    this.resultsDiv.innerHTML = '<div class="spinner-loader"></div>';
                     this.isSpinnerVisible = true;
                 }
                 this.typingTimer = setTimeout(this.getResults.bind(this), 750);
             } else {
-                this.resultsDiv.html("")
-                this.isSpinnerVisible = false
+                this.resultsDiv.innerHTML = "";
+                this.isSpinnerVisible = false;
             }
         }
-
-        this.previousValue = this.searchField.val()
+        this.previousValue = this.searchField.val();
     }
 
-    getResults() {
-        $.getJSON(wpSampleSiteData.root_url + "/wp-json/wpSampleSite/v1/search?term=" + this.searchField.val(), (results) => {
-            this.resultsDiv.html(`
+    async getResults() {
+        try {
+            const url = wpSampleSiteData.root_url + "/wp-json/wpSampleSite/v1/search?term=" + this.searchField.value;
+            const response = await axios.get(url);
+            const results = response.data;
+            this.resultsDiv.innerHTML = `
                 <div class="row">
                     <div class="one-third">
                         <h2 class="search-overlay__section-title">General Information</h2>
@@ -100,27 +110,30 @@ class Search {
                         `).join("")}
                     </div>
                 </div>
-            `);
+            `;
             this.isSpinnerVisible = false;
-        });
+        } catch (e) {
+            console.log("Error: " . e);
+        }
     }
 
     openOverlay() {
-        this.searchOverlay.addClass("search-overlay--active");
-        $("body").addClass("body-no-scroll");
-        this.searchField.val("");
+        this.searchOverlay.classList.add("search-overlay--active");
+        document.body.classList.add("body-no-scroll");
+        this.searchField.value = "";
         setTimeout(() => this.searchField.focus(), 301);
         this.isOverlayOpen = true;
+        return false;
     }
 
     closeOverlay() {
-        this.searchOverlay.removeClass("search-overlay--active")
-        $("body").removeClass("body-no-scroll")
-        this.isOverlayOpen = false
+        this.searchOverlay.classList.remove("search-overlay--active");
+        document.body.classList.remove("body-no-scroll");
+        this.isOverlayOpen = false;
     };
 
     addSearchHTML() {
-        $("body").append(`
+        document.body.insertAdjacentHTML("beforeend", `
             <div class="search-overlay">
                 <div class="search-overlay__top">
                     <div class="container">
@@ -137,4 +150,4 @@ class Search {
     }
 }
 
-export default Search
+export default Search;
